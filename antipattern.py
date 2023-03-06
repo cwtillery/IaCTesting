41bfcc95382f39cae44d12201c853a993599082c
cwtillery <carsonwtillery@gmail.com>
2023-03-06 00:22:35 -0600

    testing commit with codacity 2

--- antipattern.py
+++ antipattern.py
@@ -4,7 +4,7 @@ Created on Fri Oct  9 00:54:14 2020
 
 @author: mehedi.md.hasan
 """
-# Adding line for Commit test
+# Editing line for Commit test
 
 from abc import ABC, abstractmethod
 from util import Util
